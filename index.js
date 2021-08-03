ffff69c86ab0beeef527ea16ec4fae3543a4a523
Rakib544 <md.rakib10122003@gmail.com>
2021-08-03 22:31:20 +0600

    banner info added complete

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
 //PORT
 const PORT = 8080;
+
+//router
+const bannerRoute = require("./routes/banner");
+
+//Connecting mongoose
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connect to mongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/banner", bannerRoute);
 
 app.get("/", (req, res) => {
   res.send(" Bismillah Hello World!!!");
---
+++ models/OrderCard.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const OrderCardSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: true,
+    },
+    orderDate: {
+      type: Date,
+      default: Date.now,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["Work In Progress", "Completed"],
+    },
+    deliveryDate: {
+      type: Date,
+      required: true,
+    },
+    file: {
+      type: String,
+      default: "",
+    },
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerEmail: {
+      type: String,
+      required: true,
+    },
+    orderAmount: {
+      type: String,
+      required: true,
+    },
+    deliveryTime: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("OrderCard", OrderCardSchema);
