621fca56cd77db106c8cf5d578dca04a66896f0f
Rakib544 <md.rakib10122003@gmail.com>
2021-08-05 16:59:25 +0600

    some code changes complete

--- routes/orderCard.js
+++ routes/orderCard.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
 const OrderCard = require("../models/OrderCard");
 
-router.get("/", async (req, res) => {
+router.get("/pending", async (req, res) => {
   try {
     const post = await OrderCard.find({ orderStatus: "Work In Progress" });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+router.get("/completed", async (req, res) => {
+  try {
+    const post = await OrderCard.find({ orderStatus: "Completed" });
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json(err);
@@ -20,13 +29,13 @@ router.post("/post", async (req, res) => {
   }
 });
 
-router.put("/abc/:id", async (req, res) => {
+router.put("/update/orderStatus/:id", async (req, res) => {
   try {
     await OrderCard.findByIdAndUpdate(
       { _id: req.params.id },
       {
         $set: {
-          title: req.body.title,
+          orderStatus: req.body.orderStatus,
         },
       },
       {
