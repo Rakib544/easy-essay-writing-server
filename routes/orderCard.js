c56d78781da5e242a577c377e1580ec627a4dce0
Rakib544 <md.rakib10122003@gmail.com>
2021-08-06 10:08:30 +0600

    user route added

--- routes/orderCard.js
+++ routes/orderCard.js
@@ -19,6 +19,15 @@ router.get("/completed", async (req, res) => {
   }
 });
 
+router.post("/userOrder", async (req, res) => {
+  try {
+    const userOrder = await OrderCard.find({ customerEmail: req.body.email });
+    res.status(200).json(userOrder);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
 router.post("/post", async (req, res) => {
   try {
     const newPost = new OrderCard(req.body);
