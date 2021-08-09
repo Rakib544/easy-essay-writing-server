7d6b255d7827c44d6074e1fca6ecb74d7e6360c1
Rakib544 <md.rakib10122003@gmail.com>
2021-08-10 00:37:55 +0600

    user collection created

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,21 @@
 const router = require("express").Router();
 const Admin = require("../models/Admin");
+const User = require("../models/User");
 
 router.post("/", async (req, res) => {
   try {
     const userData = req.body;
     const email = req.body.email;
+    const name = req.body.name;
     const user = await Admin.find({ email: email });
     if (user.length === 0) {
+      const newUser = new User({ email, name, userType: "user" });
+      await newUser.save();
       userData.userType = "user";
       res.json(userData);
     } else {
+      const newUser = new User({ email, name, userType: "admin" });
+      await newUser.save();
       userData.userType = "admin";
       res.json(userData);
     }
