6be6c680f763d897b80a84a66bda004bb3079527
Rakib544 <md.rakib10122003@gmail.com>
2021-08-12 00:33:06 +0600

    affiliate router split complete

--- routes/afiliateUser.js
+++ routes/afiliateUser.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
 const AffiliateUser = require("../models/AffiliateUser");
 
-router.post("/", async (req, res) => {
-  const startDate = new Date();
-  const lastDate = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+router.post("/all", async (req, res) => {
   try {
     const email = req.body.email;
     const users = await AffiliateUser.find({ referredBy: email });
+    res.status(200).json({ allUsers: users.length });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.post("/lastSevenDays", async (req, res) => {
+  const startDate = new Date();
+  const lastDate = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+
+  try {
+    const email = req.body.email;
     const lastSevenDaysUser = await AffiliateUser.find({
       referredBy: email,
       accountCreatedAt: {
@@ -14,6 +24,16 @@ router.post("/", async (req, res) => {
         $lt: new Date(new Date(startDate).setHours(23, 59, 59)),
       },
     });
+    res.status(200).json({ lastSevenDaysUser: lastSevenDaysUser.length });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+router.post("/lastOneDay", async (req, res) => {
+  const startDate = new Date();
+  try {
+    const email = req.body.email;
     const lastOneDayUser = await AffiliateUser.find({
       referredBy: email,
       accountCreatedAt: {
@@ -21,16 +41,9 @@ router.post("/", async (req, res) => {
         $lt: new Date(new Date(startDate).setHours(23, 59, 59)),
       },
     });
-    const lastOneDaysCreatedUser = lastOneDayUser.length;
-    const lastSevenDayCreatedUsers = lastSevenDaysUser.length;
-    const allTimeCreatedUsers = users.length;
-    res.json({
-      lastOneDaysCreatedUser,
-      lastSevenDayCreatedUsers,
-      allTimeCreatedUsers,
-    });
+    res.status(200).json({ lastOneDayUser: lastOneDayUser.length });
   } catch (err) {
-    res.json(err);
+    res.status(404).json(err);
   }
 });
 
