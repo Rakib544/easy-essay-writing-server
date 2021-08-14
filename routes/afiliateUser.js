393b1706e6614db21fca866b020b7f4eba32a7ea
Rakib544 <md.rakib10122003@gmail.com>
2021-08-15 00:57:48 +0600

    pagination for affiliate user created

--- routes/afiliateUser.js
+++ routes/afiliateUser.js
@@ -57,13 +57,55 @@ router.post("/lastOneDay", async (req, res) => {
   }
 });
 
-router.get("/allAffiliateUsers", async (req, res) => {
-  try {
-    const allAffiliateUsers = await AffiliateUser.find({});
-    res.status(200).json(allAffiliateUsers);
-  } catch (err) {
-    res.status(404).json(err);
+router.post(
+  "/allAffiliateUsers",
+  getPaginatedResults(AffiliateUser),
+  async (req, res) => {
+    res.status(200).json(res.paginatedResults);
   }
-});
+);
+
+//pagination functionality done
+//middleware
+function getPaginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.body.page);
+    const limit = 10;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    try {
+      const total = await model.find({});
+      results.totalData = total.length;
+      results.result = await model
+        .find({})
+        .sort({ orderDate: -1 })
+        .limit(limit)
+        .skip(startIndex)
+        .exec();
+      res.paginatedResults = results;
+      next();
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  };
+}
 
 module.exports = router;
